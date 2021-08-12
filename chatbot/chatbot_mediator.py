0bc9f9fadd1ecc44d8ed6a134181e2a87a21ccf6
clovis-chatbotmaker <86970391+clovis-chatbotmaker@users.noreply.github.com>
2021-08-12 17:52:14 -0300

    Fixing types from chatbot_mediator.py

--- chatbot/chatbot_mediator.py
+++ chatbot/chatbot_mediator.py
@@ -1,21 +1,23 @@
-from strategies.response_strategy import ResponseStrategy
+from strategies.default_strategy import DefaultStrategy
 from predictor.default_predictor import DefaultPredictor
 
 
 class ChatbotMediator:
 
     def __init__(self, path_model):
-        self.strategies = []
+        self.__strategies = []
         self.__predictor = DefaultPredictor(path_model)
 
-    def add_strategy(self, response_generator: ResponseStrategy):
-        self.strategies.append(response_generator)
+    def add_strategy(self, strategy: DefaultStrategy):
+        if not issubclass(type(strategy), DefaultStrategy):
+            raise ValueError("An strategy must inherit from DefaultStrategy")
+        self.__strategies.append(strategy)
 
     def notify(self, message: str) -> None:
         if type(message) is not str:
             raise ValueError("message must be string")
         doc = self.__predictor.model(message)
-        for strategy in self.strategies:
+        for strategy in self.__strategies:
             if strategy.execute:
                 strategy.execute(doc)
 
